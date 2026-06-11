3e634f56d2a6d61a2e6b7f2091eb0c67625c9148
Evgeny-96 <63477629+Evgeny-96@users.noreply.github.com>
2026-06-11 11:33:36 +0500

    lesson05-dif
    Update lesson05

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
    "use strict";
 
-   const num = 266219;
-   let result = 1;
+   let arr = ['12', '21', '135', '542', '421', '256', '421'];
 
-   const numStr = num.toString();
+for (let i = 0; i < arr.length; i++) {
+  if (String(arr[i])[0] == '2' || String(arr[i])[0] == '4') {
+    console.log(arr[i]);
+  }
+}
+console.log('-------------------');
 
-   for (let i = 0; i < num.toString().length; i++) {
-      result *= numStr[i];
-   }
+const isCheckNum = function (number) {
+  if (number < 1) return false;
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
 
-   console.log('result: ', result);
-   const step = result ** 3;
-   console.log('step: ', step);
-   console.log('первые 2 цифры step: ', step.toString().slice(0, 2));
+const primeNumb = function (num) {
+  for (let i = 2; i <= num; i++) {
+    if (isCheckNum(i)) {
+      console.log('Делители этого числа: 1 и ' + i);
+    }
+  }
+}
+
+primeNumb(100);
+
 });
